cbe0b5b6d5536c97f9db1d8d729a648b6efa2297
xq <3059697831@qq.com>
2026-06-09 12:07:36 +0800

    feat: 用户注册时设置默认头像

--- novablog-server/src/main/java/com/novablog/service/impl/UserServiceImpl.java
+++ novablog-server/src/main/java/com/novablog/service/impl/UserServiceImpl.java
@@ -70,6 +70,7 @@ public class UserServiceImpl implements UserService {
         user.setUsername(username);
         user.setPassword(passwordEncoder.encode(password));
         user.setNickname(nickname);
+        user.setAvatar("https://xq945.oss-cn-beijing.aliyuncs.com/NovaBlog/first-avatar.jpg");  // 默认头像
         user.setRole("USER");   // 注册时固定为普通用户，禁止注册为管理员
         user.setStatus(1);      // 默认启用状态
 
